2f27dd5b74f07feee82e8b81f98c65b8971a726c
thinktanksec <zack.deveau@shopify.com>
2019-10-30 15:57:25 -0400

    removed session folders

--- session_01/exercises/example.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-int main() {
-  int im_a_stack_var = 2;
-  printf("This is the stack var addr: 0x%x, This is the value: %d\n", &im_a_stack_var, im_a_stack_var);
-  return 0;  
-}
--- session_01/exercises/exercise_2_spot_the_variables.c
+++
@@ -1,8 +0,0 @@
-int main() {
-  int num_of_shrimp = 5;
-  int shrimp[5] = { 2, 3, 5, 1, 3 }; 
-  int num_of_mouths = 4;
-  
-  num_of_shrimp += shrimp[2]; 
-  printf("I'm Stan the family man, I've got %d Shrimp and %d Mouths to feed.\n", num_of_shrimp, num_of_mouths);
-} 
